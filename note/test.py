08bef74bd2fe8dcef2d55735032053e56151210e
lin <lin@qq.com>
2019-10-24 17:38:48 +0800

    commit

--- note/test.py
+++ note/test.py
@@ -1,10 +1,14 @@
-from copy import deepcopy
-list1 = [[1, 2], 3, 4]
-list2 = list1.copy()
-list3 = deepcopy(list1)
-list1[0][1] = 100
-list1[1] = 200
-print(list1)
-print(list2)
-print(list3)
+# 全局变量
+c = 10
+def demo():
+    # 局部变量
+    c = 20
+print(c)
 
+# 局部变量 => 全局变量
+# global关键字
+def demo():
+    global c
+    c = 10
+demo()
+print(c)
