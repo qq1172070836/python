0b57ecba9b90f42370b8bee2d86299a637f01a5f
lin <lin@qq.com>
2019-10-24 17:01:41 +0800

    commit

--- note/test.py
+++ note/test.py
@@ -1,4 +1,14 @@
 n = 100
-while n > 0:
+# 用循环实现
+# while n > 0:
+#     n = n//2
+#     print(n)
+
+# 用函数实现
+def calc(n):
     n = n//2
     print(n)
+    if n > 0:
+        calc(n)
+    print(n)
+calc(n)
