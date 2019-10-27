36482a10af5d9c6b37d55ac313957f684cc077ca
lin <lin@qq.com>
2019-10-27 09:05:26 +0800

    commit

--- note/test.py
+++ note/test.py
@@ -1,18 +1,5 @@
-def consumer(name):
-    print('消费者{}准备吃包子。。。'.format(name))
-    while True:
-        baozi = yield
-        print('消费者{}收到包子编号：{}'.format(name, baozi))
-
-c1 = consumer('c1')
-c2 = consumer('c2')
-c3 = consumer('c3')
-c1.__next__()
-c2.__next__()
-c3.__next__()
-
-for i in range(10):
-    print('----生成了第{}批包子----'.format(i))
-    c1.send(i)
-    c2.send(i)
-    c3.send(i)
+s = '你好'
+b = s.encode('utf-8')
+print(b, type(b))
+s = b.decode('utf-8')
+print(s, type(s))
