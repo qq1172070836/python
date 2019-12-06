28c765158ce5698d2349036bb87f1edf5c525a42
lin <lin@qq.com>
2019-12-06 22:01:42 +0800

    commit

---
+++ note/test.py
@@ -0,0 +1,24 @@
+# Number
+#     int
+#         0box, int bin oct hex
+#     float
+#         1/1 => float
+#         math.ceil(1.1) => 2
+from decimal import Decimal
+c = 1.2 - 1.1
+d = Decimal('1.2') - Decimal('1.1')
+print(c, d)
+#     bool
+#         True, False
+#     complex
+#         complex(a, b), a + bj
+
+# String
+#
+# Tuple
+#
+# List
+#
+# Set
+#
+# Dict
